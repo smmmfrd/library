2b9a6fa3c8cca4476b980f04af75c5ccaf8b4a74
Sam Mumford <smmmfrd@gmail.com>
2022-07-01 15:08:43 -0700

    Add Placeholder Books

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var myLibrary = [];
 
-function Book(author, title, numPages, read){
-    this.author = author;
+function Book(title, author, numPages, read){
     this.title = title;
+    this.author = author;
     this.numPages = numPages;
     this.read = read;
 }
@@ -13,14 +13,21 @@ Book.prototype.logLine = function() {
 };
 
 // Management
-function addBookToLibrary(author, title, numPages, read){
-    let newBook = new Book(author, title, numPages, read);
+function addBookToLibrary(title, author, numPages, read){
+    let newBook = new Book(title, author, numPages, read);
 
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("Frank Herbert", "Dune", 1000, false);
-addBookToLibrary("J.R.R. Tolkien", "Twin Towers", 1000, false);
+addBookToLibrary("Dune", "Frank Herbert", 1000, false);
+addBookToLibrary("Twin Towers", "J.R.R. Tolkien", 1000, false);
+addBookToLibrary("Naruto vol. 1", "Masashi Kishimoto", 300, true);
+addBookToLibrary("Percy Jackson and the Lightning Thief", "Rick Riordan", 300, true);
+addBookToLibrary("Star Wars: Boba Fett vols. 1-6", "Terry Bisson(1-2) and Elizabeth Hand(3-6)", 600, true);
+addBookToLibrary("American Gods", "Neil Gaiman", 400, false);
+addBookToLibrary("The Mystery of Edwin Drood", "Charles Dickens", 218, false);
+addBookToLibrary("Encyclopedia Brown and the Case of the Secret Pitch", "Donald J. Sobol", 96, false);
+addBookToLibrary("Alex Rider: Stormbreaker", "Anthony Horowitz", 304, false);
 
 function logLibraries(){
     myLibrary.forEach((book) => {
